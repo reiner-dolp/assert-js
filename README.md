a06894b0003ce2ac098f54cbcf2f4cdaaae9cb7e
Reiner Dolp <reiner-dolp@users.no-reply.github.com>
2016-06-23 18:45:55 +0100

    Initial commit, v0.0.1

---
+++ README.md
@@ -0,0 +1,83 @@
+__A very simple assertion library to ensure pre conditions, post conditions and
+invariants are adhered to.__
+
+---
+Note that this is not a package for _[node.js](https://nodejs.org/api/assert.html)_. _node.js_
+has its own [assertion package](https://nodejs.org/api/assert.html).
+---
+
+
+## API
+
+All assertion methods take the variable to test as first argument, and an
+optional error message as second argument:
+
+```js
+function assert(condition, msg)
+assert.is_int(val, msg)
+assert.is_undefined(val, msg)
+assert.is_func(val, msg)
+assert.is_numeric(val, msg)
+assert.is_array(val, msg)
+assert.is_string(val, msg)
+assert.is_object(val, msg)
+assert.is_ctor (self, selfname)
+assert.not_reached(msg)
+```
+
+```js
+function is_int(val) { "use strict";
+function is_array(val) { "use strict";
+function is_func(val) { "use strict";
+function is_typed_array(val) { "use strict";
+function is_string(val) { "use strict";
+```
+
+## Example Usage
+
+```js
+/**
+ * Instantiate a new data object representing a rectangular raster image
+ * region.
+ *
+ * All options given can later be read using the same property name.
+ * Note that the constructor can be used as copy constructor:
+ *
+ * ```js
+ * new ImgRegion(new ImageRegion({ /* ... */ }));
+ * ```
+ *
+ * @param {int} opts.width   the width of the image region
+ *
+ * @param {int} opts.height  the height of the image region
+ * 
+ * @param {int} opts.offsetX the position of the image region
+ *                           relative to origin in x-direction.
+ *
+ * @param {int} opts.offsetY the position of the image region
+ *                           relative to origin in y-direction.
+ */
+function ImgRegion(opts) {
+	assert.is_ctor(this, ImgRegion);
+	assert.is_object(opts);
+	assert.is_int(opts.width);
+	assert.is_int(opts.height);
+	assert.is_int(opts.offsetX);
+	assert.is_int(opts.offsetY);
+	assert(opts.width >= 0);
+	assert(opts.height >= 0);
+
+	this.width   = opts.width;
+	this.height  = opts.height;
+	this.offsetX = opts.offsetX;
+	this.offsetY = opts.offsetY;
+}
+```
+
+`assert.is_ctor` ensures that the function is called using the `new` keyword.
+
+## Using an EventEmitter
+
+If you include an EventEmitter package (e.g. [1] [2] [3]) first, you can call
+`assert.eventEmitter.addListener("assertion failure", myLoggerFunc)`, where
+`myLoggerFunc` is one of your functions. 
---
+++ assert.js
@@ -0,0 +1,127 @@
+/* # Assertions and Typing
+ *
+ * ## Datatype Testing
+ *
+ * All type-testing-methods take single variable to test as argument.
+ *
+ * - is_int
+ * - is_array
+ * - is_func
+ * - is_typed_array
+ * - is_string
+ *
+ * ## Overview Assertion Methods
+ *
+ * All assertion methods take the variable to test as first argument, and an optional
+ * error message as second argument:
+ *
+ * - assert
+ * - assert.not_reached
+ * - assert.is_ctor
+ * - assert.is_int
+ * - assert.is_numeric
+ * - assert.is_array
+ * - assert.is_object
+ * - assert.is_string
+ */
+function assert(condition, msg) { "use strict";
+
+	if (!condition) {
+		msg = msg || "";
+
+		assert.is_string(msg, "second argument to assert must be a string");
+
+		msg = "[ASSERTION FAILED] " + msg;
+
+		var error;
+
+		if (typeof Error !== "undefined") {
+			error = new Error(msg);
+		}
+
+		if(assert.eventEmitter) {
+			assert.eventEmitter.emit("assertion failure", error || msg);
+		}
+
+		throw error || msg;
+	}
+}
+
+if(typeof EventEmitter !== "undefined") {
+	assert.eventEmitter = new EventEmitter();
+}
+
+function is_int(val) { "use strict";
+	return ~~val === val;
+}
+function is_array(val) { "use strict";
+	return Object.prototype.toString.call(val) === '[object Array]';
+}
+
+function is_func(val) { "use strict";
+	return val && val.constructor && val.call && val.apply;
+}
+
+function is_typed_array(val) { "use strict";
+	return val instanceof Int8Array ||
+	       val instanceof Uint8Array ||
+	       val instanceof Int8Array ||
+	       val instanceof Uint8Array ||
+	       val instanceof Uint8ClampedArray ||
+	       val instanceof Int16Array ||
+	       val instanceof Uint16Array ||
+	       val instanceof Int32Array ||
+	       val instanceof Uint32Array ||
+	       val instanceof Float32Array ||
+	       val instanceof Float64Array;
+}
+
+function is_string(val) { "use strict";
+	return typeof val === "string" || val instanceof String;
+}
+
+assert.not_reached = function(msg) { "use strict";
+	assert(false, msg || "reached code that is asserted to never be reached");
+};
+
+assert.is_int = function(val, msg) { "use strict";
+	assert(is_int(val), msg || "expected variable to be an integer");
+};
+
+assert.is_undefined = function(val, msg) { "use strict";
+	assert(typeof val === "undefined", msg || "expected variable to be undefined");
+};
+
+assert.is_func = function(val, msg) { "use strict";
+	assert(is_func(val), msg || "expected variable to be a function");
+};
+
+assert.is_function = assert.is_func;
+
+assert.is_numeric = function(val, msg) { "use strict";
+	// more permissive with strings etc:
+	//!isNaN(parseFloat(n)) && isFinite(n);
+	assert(typeof val === "number", msg || "expected number, got " + typeof val);
+};
+
+assert.is_num = assert.is_numeric;
+
+assert.is_array = function(val, msg) { "use strict";
+	assert(is_array(val) || is_typed_array(val), msg || "expected variable to be of type array");
+};
+
+assert.is_arr = assert.is_array;
+
+assert.is_string = function(val, msg) { "use strict";
+	assert(typeof val === 'string', msg || "expected variable to be of type string. Got " + typeof val + "instead.");
+};
+
+assert.is_str = assert.is_string;
+
+assert.is_object = function(val, msg) { "use strict";
+	assert(val !== null && typeof val === 'object', msg || "expected variable to be of type object");
+};
+
+assert.is_ctor = function (self, selfname) { "use strict";
+	assert(self instanceof selfname, "function <" + selfname + "> has to be called as constructor");
+};
