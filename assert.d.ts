f730aa7544c6a8563fb4d8720d3acf2b6482a712
Reiner Dolp <reiner-dolp@users.noreply.github.com>
2017-01-24 10:12:16 +0000

    Updating macro definitions

--- assert.d.ts
+++ assert.d.ts
@@ -11,7 +11,7 @@ export function ASSERT_IS_STRING(v :any, msg? :string): void;
 export function ASSERT_IS_UNDEF(v :any, msg? :string): void;
 export function ASSERT_IS_OBJ(v :any, msg? :string): void;
 export function ASSERT_IS_NUM(v :any, msg? :string): void;
-export function ASSERT_IS_CTOR(v :any, msg? :string): void;
+export function ASSERT_IS_CTOR(a :any, b :any, msg? :string): void;
 
 export function is_int(v :any): boolean;
 export function is_non_negative_int(v :any): boolean;
@@ -22,5 +22,5 @@ export function is_string(v :any): boolean;
 export function is_undef(v :any): boolean;
 export function is_obj(v :any): boolean;
 export function is_num(v :any): boolean;
-export function is_ctor(v :any): boolean;
+export function is_ctor(a :any, b :any): boolean;
 export function is_typed_array(v :any): boolean;
